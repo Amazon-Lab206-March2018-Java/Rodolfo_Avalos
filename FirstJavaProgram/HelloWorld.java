a03606898870861e5453e40a2c2cb7fb30936a93
Avalos Leon <rodoleon@amazon.com>
2018-04-23 11:25:23 -0600

    first commit

---
+++ FirstJavaProgram/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("My name is Rodolfo Avalos");
+        System.out.println("I am 40 years old");
+        System.out.println("My hometown is San Jose, Costa Rica");
+    }
+}
